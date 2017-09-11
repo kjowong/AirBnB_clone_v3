9daf2da9e5c01b5e53054eccb6689d3cfc773fbb
Kimberly Wong <kjowong@gmail.com>
2017-09-11 14:41:21 +0000

    Adding post in state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,14 +2,16 @@
 """
     states.py file in v1/views
 """
-from flask import abort, Flask, jsonify
+from flask import abort, Flask, jsonify, request
 from api.v1.views import app_views
 from models import storage
 from models.base_model import BaseModel
+from models.state import State
 
 # is this next line needed? we are never using app, only app_views
 app = Flask(__name__)
 to_json = BaseModel.to_json
+
 
 @app_views.route("/states", methods=['GET'])
 def get_all_states():
@@ -21,9 +23,9 @@ def get_all_states():
     # Get dictionary of all State objects
     all_states = storage.all('State')
     # Loop through values in the dictionary
-    for object in all_states.values():
+    for state_object in all_states.values():
         # to_json() the object and append to the list
-        state_list.append(to_json(object))
+        state_list.append(to_json(state_object))
     # jsonify the list and return
     return (jsonify(state_list))
 
@@ -63,3 +65,20 @@ def delete_a_state(state_id):
         response.status = 200
         # return response
         return response
+
+
+@app_views.route("/states", methods=['POST'])
+def create_a_state():
+    """
+        Method to create a state
+    """
+    if request.method == "POST":
+        json_data = request.get_json()
+        if not json_data:
+            return jsonify({"message": "Not a JSON"}), 400
+        if "name" not in json_data:
+            return jsonify({"message": "Missing name"}), 400
+        new_state = State(**json_data)
+        new_state.save()
+        json_state = new_state.to_json()
+    return jsonify(json_state), 201
