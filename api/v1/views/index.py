8258be148043ef728b9d8dcd83533c280087211e
KRLoyd <loydkristen@gmail.com>
2017-09-12 20:21:11 +0000

    Add route to /api/v1/stats that returns a jsonified dict of the count of objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,14 +4,36 @@
 """
 from flask import Flask, jsonify
 from api.v1.views import app_views
+from models import storage
 app = Flask(__name__)
 
 
 @app_views.route("/status")
 def status_message():
+    """method to return an OK status"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route("/api/v1/stats")
 def count_classes():
-
+    """
+        Method to return a jsonified dictionary of stats.
+    """
+    # Get the count of each Class
+    amenities_count = storage.count('Amenity')
+    cities_count = storage.count('City')
+    places_count = storage.count('Place')
+    reviews_count = storage.count('Review')
+    states_count = storage.count('State')
+    users_count = storage.count('User')
+    # Create dictionary
+    stat_dict = {}
+    # Add to the dictionary
+    stat_dict["amenities"] = amenities_count
+    stat_dict["cities"] = cities_count
+    stat_dict["places"] = places_count
+    stat_dict["reviews"] = reviews_count
+    stat_dict["states"] = states_count
+    stat_dict["users"] = users_count
+    # jsonify and return dictionary
+    return jsonify(stat_dict)
