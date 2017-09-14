401fe51963ab983f41b1cb11803d909a2aa162b0
KRLoyd <loydkristen@gmail.com>
2017-09-14 18:20:35 +0000

    Complete post_a_place()

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -58,14 +58,38 @@ def post_a_place(city_id):
     """
         Method to create a Place object
     """
+
     # if City object doesn't exist, raise a 404 error
-    # use request.get_json() to transform HTTP request to a dictionary
-    # if HTTP request is not valid JSON, raise a 400 error with message "Not a JSON"
-    # if request doesn't contain key 'user_id', raise a 400 error with message "Missing user_id"
+    if not storage.get("City", city_id):
+        abort(404)
+    # Transform HTTP request to a dictionary
+    json_data = request.get_json()
+    # if HTTP request is not valid JSON, raise a 400 error
+    if not json_data:
+        return jsonify({"message": "Not a JSON"}), 400
+    # if request doesn't contain key 'user_id', raise a 400 error
+    user_id = json_data.get("user_id")
+    if not user_id:
+        return jsonify({"message": "Missing user_id"}), 400
     # if User doesn't exist, raise a 404 error
-    # if request doesn't contain key 'name', raise a 400 error with message "Missing name"
-    # Return new PLase with status code 201
-    pass
+    if not storage.get("User", user_id):
+        abort(404)
+    # if request doesn't contain key 'name', raise a 400 error
+    if not json_data.get("name"):
+        return jsonify({"message": "Missing name"}), 400
+    # Create a new_place_dict to hold all info for Place object
+    new_place_dict = {}
+    # Update new_place_dict with dictionary from request
+    new_place_dict.update(json_data)
+    # Add city_id to the dictionary
+    new_place_dict["city_id"] = city_id
+    # Create and save new Place object
+    new_place = Place(**new_place_dict)
+    new_place.save()
+    # to_json() the Place object
+    json_place = new_place.to_json()
+    # Return new Place with status code 201
+    return jsonify(json_place), 201
 
 
 @app_views.route('/places/<string:place_id>', methods=['PUT'])
